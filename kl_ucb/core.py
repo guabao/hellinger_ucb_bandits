ce7f28ec0e7553019a7ef4bb7de32edf74a2724e
jiazhou wang <jiazhouwang@jiazhous-MacBook-Pro.local>
2020-07-14 10:43:54 -0700

    fix kl ucb bug

--- kl_ucb/core.py
+++ kl_ucb/core.py
@@ -11,12 +11,13 @@ class KLUCB(object):
     def __init__(self, user_feature_hash, content_hash, convex_func=None, eps=1e-6):
         self.user_bin = user_feature_hash
         self.content_bin = content_hash
-        self.total_reward = numpy.zeros([self.user_bin, self.content_bin])
-        self.trials_arm = 1e-8 * numpy.ones([self.user_bin, self.content_bin])
+        self.total_reward = 1e-8 * numpy.ones([self.user_bin, self.content_bin])
+        self.trials_arm = 2e-8 * numpy.ones([self.user_bin, self.content_bin])
         self.trials = numpy.ones(self.user_bin)
         self.parameter = {"p": numpy.zeros([self.user_bin, self.content_bin])}
         self.parameter["f(t)"] = numpy.log if convex_func is None else convex_func
         self.parameter["eps"] = eps
+        self.parameter["q"] = numpy.ones([self.user_bin, self.content_bin]) - 1e-3
         return None
 
     def update_reward_single_trial(self, user_hash, content_hash, reward):
@@ -30,19 +31,19 @@ class KLUCB(object):
 
     def trial(self, user_hash):
         # if no pull yet, pull
-        if numpy.any(self.trials_arm):
-            return numpy.argsort(self.trials_arm, axis=1)
+        if numpy.any(self.trials_arm[user_hash] < 1):
+            return numpy.argsort(self.trials_arm[user_hash])
         # update parameter
         eps = self.parameter["eps"]
-        p = self.total_reward / self.trials_arm
+        p = self.total_reward[user_hash] / self.trials_arm[user_hash]
         f = self.parameter["f(t)"]
-        self.parameter["p"] = p
-        c = f(self.trials) / self.trials_arm
+        self.parameter["p"][user_hash] = p
+        c = f(self.trials[user_hash]) / self.trials_arm[user_hash]
         # newton root finding based on KL divergence
         rhs = p * numpy.log(p) + (1 - p) * numpy.log(1 - p) - c
-        err = numpy.ones([self.user_bin, self.content_bin])
-        flag = numpy.ones([self.user_bin, self.content_bin], dtype=bool)
-        x = p if 'q' not in self.parameter else self.parameter['q']
+        err = numpy.ones([self.content_bin])
+        flag = numpy.ones([self.content_bin], dtype=bool)
+        x = self.parameter['q'][user_hash]
         while numpy.any(flag):
             p1 = p[flag]
             x1 = x[flag]
@@ -52,8 +53,9 @@ class KLUCB(object):
             fprime = p1 / x1 - (1 - p1) / (1 - x1)
             dx = fx / fprime
             flag1[numpy.abs(dx) < eps] = False
+            x1 = numpy.maximum(numpy.minimum(x1 - dx, 1 - 1e-8), p1 + 1e-8)
+            x[flag] = x1
             flag[flag]  = flag1
-            x = numpy.max(numpy.min(x - dx, 1 - 1e-8), p + 1e-8)
 
-        self.parameter['q'] = x
-        return numpy.argsort(self.parameter['q'])
+        self.parameter['q'][user_hash] = x
+        return numpy.argsort(self.parameter['q'][user_hash])
